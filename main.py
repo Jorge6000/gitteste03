d208dfb8ed71647f64ef0c0849d7ac7b1989041e
JorgeH <jorginhomocelin@gmail.com>
2026-02-22 16:50:59 -0300

    initial commit

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import numpy as np
-import pygame
-a = np.dtypes
+import pygame as pg
+import pygame.event
 
-
-
+pg.init()
+print("Setup Start ")
+window = pg.display.set_mode(size=(600, 480))
+print("Setup End")
+print("Loop Start")
+while True:
+    # Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()  # Close Window
+            quit()  # end pygame
